f4b4a9a93cf7e7f8e28899c7faf94d442dcb3008
Kazim Farid <56271434+KazimF@users.noreply.github.com>
2021-05-09 19:55:36 +0530

    Update insert2.php

--- insert2.php
+++ insert2.php
@@ -11,18 +11,24 @@
 
 if (!empty($Name) || !empty($Highest_Qualification) || !empty($Year_Of_Passing) || !empty($Gender) ||
 !empty($Email) || !empty($Contact_No) || !empty($Course_Enroll) || !empty($Course_Type) || !empty($Preferred_Location)){
+  // $host = "localhost";
+  // $dbUsername = "root";
+  // $dbPassword = "1234";
+  // $dbname = "Demo";
+
   $host = "remotemysql.com";
-  $dbUsername = "fwlMeZ9mfD";
-  $dbPassword = "JGQSOqLVpW";
-  $dbname = "fwlMeZ9mfD";
+   $dbUsername = "fwlMeZ9mfD";
+   $dbPassword = "JGQSOqLVpW";
+   $dbname = "fwlMeZ9mfD";
+
 
   $con = new mysqli($host, $dbUsername,$dbPassword, $dbname);
   if(mysqli_connect_error()){
     die('Connection Error('. mysqli_connect_error().')'. mysqli_connect_error());
   }else {
     $SELECT = " SELECT email From Submit Where email = ? Limit 1";
-    $INSERT = "INSERT Into Submit (Name,Highest_Qualification,Year_Of_Passing,Gender,Email,Contact_No,Course_Enroll,Course_Type, Preferred_Location) values ('$Name','$Highest_Qualification','$Year_Of_Passing','$Gender','$Email','$Contact_No','$Course_Enroll','$Course_Type','$Preferred_Location');";
     //$INSERT = "INSERT Into Submit (Name,Highest_Qualification,Year_Of_Passing,Gender,Email,Contact_No,Course_Enroll,Course_Type, Preferred_Location) values (?,?,?,?,?,?,?,?,?)";
+    $INSERT = "INSERT Into Submit (Name,Highest_Qualification,Year_Of_Passing,Gender,Email,Contact_No,Course_Enroll,Course_Type, Preferred_Location) values ('$Name','$Highest_Qualification','$Year_Of_Passing','$Gender','$Email','$Contact_No','$Course_Enroll','$Course_Type','$Preferred_Location');";
 
     $stmt = $con->prepare($SELECT);
     $stmt->bind_param("s", $Email);
